f657ce18bc7d4d097440d275c1deb111440ee591
ShivangiShaliniJames <L00161902@atu.ie>
2024-11-21 17:21:08 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,54 @@
 provider "aws" {
-  region  = var.aws_region
-  assume_role {
-    role_arn = var.role_arn
-    session_name = "github-actions-session"
-    web_identity_token = file("${env:TERRAFORM_WEB_IDENTITY_TOKEN_FILE}")
+  region = var.aws_region
+}
+
+data "tls_certificate" "tfc_certificate" {
+  url = "https://${var.tfc_hostname}"
+}
+
+resource "aws_iam_openid_connect_provider" "stacks_openid_provider" {
+  url            = "https://${var.tfc_hostname}"
+  client_id_list = ["aws.workload.identity"]
+
+  thumbprint_list = [data.tls_certificate.tfc_certificate.certificates[0].sha1_fingerprint]
+}
+
+resource "aws_iam_role" "stacks_role" {
+  name               = substr(replace("stacks-${var.tfc_organization}-${var.tfc_project}", "/[^\\w+=,.@-]/", "-"), 0, 64)
+  assume_role_policy = data.aws_iam_policy_document.stacks_role_policy.json
+}
+
+data "aws_iam_policy_document" "stacks_role_policy" {
+  statement {
+    effect = "Allow"
+    principals {
+      type        = "Federated"
+      identifiers = [aws_iam_openid_connect_provider.stacks_openid_provider.arn]
+    }
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+    condition {
+      test     = "StringEquals"
+      variable = "app.terraform.io:aud"
+      values   = ["aws.workload.identity"]
+    }
+    condition {
+      test     = "StringLike"
+      variable = "app.terraform.io:sub"
+      values   = ["organization:${var.tfc_organization}:project:${var.tfc_project}:stack:*:*"]
+    }
   }
 }
 
-resource "aws_s3_bucket" "example" {
-  bucket = "example-terraform-identity-tokens"
-  acl    = "private"
+resource "aws_iam_role_policy_attachment" "iam" {
+  role       = aws_iam_role.stacks_role.name
+  policy_arn = "arn:aws:iam::aws:policy/IAMFullAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "sudo" {
+  role       = aws_iam_role.stacks_role.name
+  policy_arn = "arn:aws:iam::aws:policy/PowerUserAccess"
+}
+
+output "role_arn" {
+  value = aws_iam_role.stacks_role.arn
 }
